9f5220f1257bd04ab90f81c86f2576fd2e78349a
Crowdbotics <team@crowdbotics.com>
2020-10-24 21:42:13 +0000

    Update models

--- animation_pc_moblie_21921/wsgi.py
+++ animation_pc_moblie_21921/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animation_pc_moblie_21921.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "animation_pc_moblie_21921.settings")
 
 application = get_wsgi_application()
